8fa5c7d236df509288a6c6ad7fa2ebe6f86ae1fd
Atharva <ATHARVAPARANDEYTL@GMAIL.COM>
2026-02-21 12:39:53 +0530

    Delete Projects directory

--- Projects/README.md
+++
@@ -1,1 +0,0 @@
-This repository is a collection of my experiments, explorations, and practical implementations across a wide range of technical domains. It serves as a living portfolio where I document what I build, test, analyze, and learn.
--- Projects/pn-missile-sim/Project-Missile_Defense_sim copy/radar.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from config import RADAR_NOISE_STD
-
-class Radar:
-    def measure(self, true_position):
-        # Add Gaussian noise to simulate imperfect measurements
-        noise = np.random.normal(0, RADAR_NOISE_STD, size=2)
-        return true_position + noise
